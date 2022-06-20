6870ffff831a604138a412cf4874f7bbac5138f6
Scarrrt <107841869+Scarrrt@users.noreply.github.com>
2022-06-20 15:38:07 +0700

    Create replit.nix

---
+++ replit.nix
@@ -0,0 +1,8 @@
+{ pkg }: {
+	dep  = [
+		pkg . nodejs-16_x
+        pkg . paket node . TypeScript-Bahasa-Server
+        pkg . paket node . benang
+        pkg . paket ulang . bersenda gurau
+	];
+}
